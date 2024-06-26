9829f5542b795f7531eda6c668f8ee0306fdca15
Krystian Safjan <ksafjan@gmail.com>
2024-06-26 15:00:53 +0200

    test: add more tests

--- tests/e2e/test_main.py
+++ tests/e2e/test_main.py
@@ -35,13 +35,103 @@ def test_main():
         ranks=my_ranks,
         step_labels=my_step_labels,
         chunk_labels=my_chunk_labels,
-        fig_size=(6, 6),
         title_font_size=24,
     )
     _ = rf.plot()
-    plt.savefig("../img/rankflow.png")
+    # plt.savefig("../img/rankflow.png")
     plt.show()
 
 
-if __name__ == "__main__":
-    test_main()
+def test_df_basic():
+    import pandas as pd
+
+    data = {"Doc 1": [2, 1, 3, 2], "Doc 2": [1, 2, 1, 3], "Doc 3": [3, 3, 2, 1]}
+    df = pd.DataFrame(data, index=["Step_1", "Step_2", "Step_3", "Step_4"])
+
+    rf = RankFlow(df=df)
+    _ = rf.plot()
+    # save
+    plt.savefig("./rankflow_basic_pandas.png")
+    plt.show()
+
+
+def test_df_1_col__many_ranks():
+    import pandas as pd
+
+    data = {"A": [1, 2, 1, 3]}
+    df = pd.DataFrame(data, index=["step_1", "step_2", "step_3", "step_4"])
+
+    rf = RankFlow(df=df)
+    _ = rf.plot()
+    plt.show()
+
+
+def test_df_1_col__1_rank():
+    import pandas as pd
+
+    data = {"A": [1, 1, 1, 1]}
+    df = pd.DataFrame(data, index=["step_1", "step_2", "step_3", "step_4"])
+
+    rf = RankFlow(df=df)
+    _ = rf.plot()
+    plt.show()
+
+
+
+def test_df_two_steps():
+    import pandas as pd
+
+    data = gen_data(n_docs=3, n_steps=2)
+    df = pd.DataFrame(data, index=["Step_1", "Step_2"])
+
+    rf = RankFlow(df=df)
+    _ = rf.plot()
+    plt.show()
+
+
+def gen_data(n_docs: int, n_steps):
+    """Generate data for n_docs documents and n_steps steps.
+
+    E.g. for 3 documents and 4 steps:
+    data = {"Doc 1": [2, 1, 3, 2], "Doc 2": [1, 2, 1, 3], "Doc 3": [3, 3, 2, 1]}
+
+    Note that the values in the rows are unique.
+    use seed=42 for reproducibility.
+    """
+    np.random.seed(42)
+    # create empty matrix
+    data_matrix = np.zeros((n_steps, n_docs))
+    # generate matrix where each row is a permutation of 1, 2, ..., n_docs
+    for i in range(n_steps):
+        data_matrix[i, :] = np.random.permutation(n_docs)
+    # convert to integer
+    data_matrix = data_matrix.astype(int)
+
+    # shift ranks by +1
+    data_matrix += 1
+
+    # create dictionary with column names as keys and rows as values
+    data = {f"Doc {i+1}": data_matrix[:, i] for i in range(n_docs)}
+    return data
+
+
+def test_df_many_rows():
+    import pandas as pd
+
+    # generate data for 1000 documents
+    n_docs = 50
+    n_steps = 3
+    data = gen_data(n_docs=n_docs, n_steps=n_steps)
+    df = pd.DataFrame(data, index=["Step_1", "Step_2", "Step_3"])
+
+    my_ranks = df.to_numpy()
+    my_step_labels = df.index.to_list()
+    my_chunk_labels = df.columns.to_list()
+    rf = RankFlow(
+        ranks=my_ranks,
+        step_labels=my_step_labels,
+        chunk_labels=my_chunk_labels,
+        title_font_size=24,
+    )
+    _ = rf.plot()
+    plt.show()
